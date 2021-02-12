9961c5efccebae6c185ed6c7c1461b98f814b18c
Yuriuh <642192014@qq.com>
2021-02-13 00:02:45 +0800

    连接mongodb

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const connectDB = require('./config/db')
+
+// 路由
 const course = require('./routes/course')
 
 dotenv.config({
   path: './config/config.env',
 })
+
+connectDB()
 
 const app = express()
 
@@ -21,9 +26,16 @@ app.use('/api/v1/course', course)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.magenta
       .bold
   )
+})
+
+process.on('unhandledRejection', err => {
+  console.log(`Error: ${err.message}`)
+  server.close(() => {
+    process.exit(1)
+  })
 })
