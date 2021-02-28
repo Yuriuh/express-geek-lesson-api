3f7940dad95e3acde2c9c5c972088fe3f28f0af4
Yuriuh <642192014@qq.com>
2021-02-28 16:10:53 +0800

    增加异步中间件

--- controllers/course.js
+++ controllers/course.js
@@ -1,108 +1,89 @@
 const Course = require('../models/Course')
 const ErrorResponse = require('../utils/errorResponse')
+const asnycHandler = require('../middleware/async')
 
 /**
  * @desc   获取所有课程
  * @route  GET /api/v1/course
  * @access 公开的
  */
-exports.getCourseAll = async (req, res, next) => {
-  try {
-    const courses = await Course.find()
-    const count = courses.length
-    res.status(200).json({ success: true, count, data: courses })
-  } catch (error) {
-    res.status(200).json({ success: true, error })
-  }
-}
+exports.getCourseAll = asnycHandler(async (req, res, next) => {
+  const courses = await Course.find()
+  const count = courses.length
+  res.status(200).json({ success: true, count, data: courses })
+})
 
 /**
  * @desc   创建课程
  * @route  POST /api/v1/course
  * @access 公开的
  */
-exports.createCourse = async (req, res, next) => {
-  try {
-    const course = await Course.create(req.body)
-    res.status(200).json({ success: true, data: course })
-  } catch (error) {
-    next(error)
-  }
-}
+exports.createCourse = asnycHandler(async (req, res, next) => {
+  const course = await Course.create(req.body)
+  res.status(200).json({ success: true, data: course })
+})
 
 /**
  * @desc   查看某个课程
  * @route  GET /api/v1/course/:id
  * @access 公开的
  */
-exports.getCourse = async (req, res, next) => {
+exports.getCourse = asnycHandler(async (req, res, next) => {
   const id = req.params.id
-  try {
-    const course = await Course.findById(id)
-    const error = new ErrorResponse(
-      `Resource not found with the value of ${id}`,
-      404
-    )
-    if (!course) {
-      return next(error)
-    }
+  const course = await Course.findById(id)
+  const error = new ErrorResponse(
+    `Resource not found with the value of ${id}`,
+    404
+  )
 
-    res.status(200).json({ success: true, data: course })
-  } catch (error) {
-    next(error)
+  if (!course) {
+    return next(error)
   }
-}
+
+  res.status(200).json({ success: true, data: course })
+})
 
 /**
  * @desc   更新课程
  * @route  PUT /api/v1/course/:id
  * @access 公开的
  */
-exports.updateCourse = async (req, res, next) => {
+exports.updateCourse = asnycHandler(async (req, res, next) => {
   const id = req.params.id
   const body = req.body
-  try {
-    const course = await Course.findByIdAndUpdate(id, body, {
-      new: true,
-      runValidators: true,
-    })
+  const course = await Course.findByIdAndUpdate(id, body, {
+    new: true,
+    runValidators: true,
+  })
 
-    const error = new ErrorResponse(
-      `Resource not found with the value of ${id}`,
-      404
-    )
+  const error = new ErrorResponse(
+    `Resource not found with the value of ${id}`,
+    404
+  )
 
-    if (!course) {
-      return next(error)
-    }
-
-    res.status(200).json({ success: true, data: course })
-  } catch (error) {
-    next(error)
+  if (!course) {
+    return next(error)
   }
-}
+
+  res.status(200).json({ success: true, data: course })
+})
 
 /**
  * @desc   课程课程
  * @route  DELETE /api/v1/course/:id
  * @access 公开的
  */
-exports.deleteCourse = async (req, res, next) => {
+exports.deleteCourse = asnycHandler(async (req, res, next) => {
   const id = req.params.id
-  try {
-    const course = await Course.findByIdAndDelete(id)
+  const course = await Course.findByIdAndDelete(id)
+  const error = new ErrorResponse(
+    `Resource not found with the value of ${id}`,
+    404
+  )
 
-    const error = new ErrorResponse(
-      `Resource not found with the value of ${id}`,
-      404
-    )
-
-    if (!course) {
-      return next(error)
-    }
-
-    res.status(200).json({ success: true, data: {} })
-  } catch (error) {
-    next(error)
+  if (!course) {
+    return next(error)
   }
-}
+
+  res.status(200).json({ success: true, data: {} })
+})
