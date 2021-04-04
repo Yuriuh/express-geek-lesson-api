ca4714f4442e49958dca9a710c9f71feed7cf4e4
Yuriuh <642192014@qq.com>
2021-04-05 02:53:13 +0800

    增加用户与训练营的数据关联

--- controllers/camps.js
+++ controllers/camps.js
@@ -17,6 +17,15 @@ exports.getCamps = asnycHandler(async (req, res, next) => {
  * @access public
  */
 exports.createCamp = asnycHandler(async (req, res, next) => {
+  // 从中间件获取 user
+  req.body.user = req.user.id
+  // 如果用户角色是 admin，那么可以创建多个机构信息，否则只能创建一个机构
+  const publishedCamp = await Camp.findOne({ user: req.user.id })
+
+  if (publishedCamp && req.user.role !== 'admin') {
+    return next(new ErrorResponse('该机构已存在，请不要重复创建', 400))
+  }
+
   const camp = await Camp.create(req.body)
   res.status(200).json({ success: true, data: camp })
 })
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({
 
 const Camp = require('./models/Camp')
 const Course = require('./models/Course')
+const User = require('./models/User')
 
 // 连接数据库
 mongoose.connect(process.env.LOC_MONGO_URI, {
@@ -27,10 +28,15 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 )
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+)
+
 const insertData = async () => {
   try {
     await Camp.create(camps)
     await Course.create(courses)
+    await User.create(users)
     console.log('数据存储成功'.green.inverse)
     process.exit()
   } catch (error) {
@@ -43,6 +49,7 @@ const deleteData = async () => {
   try {
     await Camp.deleteMany()
     await Course.deleteMany()
+    await User.deleteMany()
     console.log('数据删除成功'.red.inverse)
     process.exit()
   } catch (error) {
